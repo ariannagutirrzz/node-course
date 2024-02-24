32507baf653e97f4a4660aa6de63c50c76745570
Arianna Vega <105892509+ariannagutirrzz@users.noreply.github.com>
2024-02-24 16:33:21 -0400

    Revert "fixing"

--- clase-4/utils.js
+++
@@ -1,8 +0,0 @@
-// One way to read a json in ESModules
-// import fs from 'node:fs'
-// const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
-
-// Another way to read a json in ESModules (recommended)
-import { createRequire } from 'node:module'
-const require = createRequire(import.meta.url)
-export const readJSON = (path) => require(path)
