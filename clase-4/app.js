6d51154dd6ae75ebdade633cac108a074a46b2d1
Arianna Vega <avega@equaldata.com>
2024-03-07 11:10:29 -0400

    merge with develop

--- clase-4/app.js
+++ clase-4/app.js
@@ -1,16 +1,16 @@
-import express, { json } from 'express'
-import { corsMiddleware } from './middlewares/cors.js'
-import { moviesRouter }from './routes/movies.js'
+import express, { json } from "express";
+import { corsMiddleware } from "./middlewares/cors.js";
+import { moviesRouter } from "./routes/movies.js";
 
-const app = express()
-app.use(json())
-app.use(corsMiddleware())
-app.disable('x-powered-by')
+const app = express();
+app.use(json());
+app.use(corsMiddleware());
+app.disable("x-powered-by");
 
-app.use('/movies', moviesRouter)
+app.use("/movies", moviesRouter);
 
-const PORT = process.env.PORT ?? 1234
+const PORT = process.env.PORT ?? 1234;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`)
-})
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
