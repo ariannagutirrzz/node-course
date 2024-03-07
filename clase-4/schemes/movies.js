5407dd46b5fde39e3cb238ba8c4cc493252507aa
Arianna Vega <avega@equaldata.com>
2024-03-07 10:12:30 -0400

    fix

--- clase-4/schemes/movies.js
+++ clase-4/schemes/movies.js
@@ -1,24 +1,37 @@
-import zod from 'zod'
+import zod from "zod";
 // For validations we can use a library like Zod or Joi
-  const movieSchema = zod.object({
-    title: zod.string( {
-        'message': 'Title is required and must be a string.'
-    }),
-    director: zod.string(),
-    genre: zod.array(zod.enum(['Action', 'Comedy', 'Drama', 'Horror', 'Sci-fi', 'Fantasy', 'Thriller', 'Terror', 'Crime'])),
-    duration: zod.number().min(1),
-    year: zod.number().int().min(1900).max(2024),
-    rate: zod.number().min(0).max(10).default(5),
-    poster: zod.string({
-        'message': 'Poster is required and must be a url.'
-    }).url()
+export const movieSchema = zod.object({
+  title: zod.string({
+    message: "Title is required and must be a string.",
+  }),
+  director: zod.string(),
+  genre: zod.array(
+    zod.enum([
+      "Action",
+      "Comedy",
+      "Drama",
+      "Horror",
+      "Sci-fi",
+      "Fantasy",
+      "Thriller",
+      "Terror",
+      "Crime",
+    ])
+  ),
+  duration: zod.number().min(1),
+  year: zod.number().int().min(1900).max(2024),
+  rate: zod.number().min(0).max(10).default(5),
+  poster: zod
+    .string({
+      message: "Poster is required and must be a url.",
+    })
+    .url(),
 });
 
 export function validateMovie(input) {
-    return movieSchema.safeParse(input);
+  return movieSchema.safeParse(input);
 }
 
 export function validatePartialMovie(input) {
-    return movieSchema.partial().safeParse(input);
+  return movieSchema.partial().safeParse(input);
 }
-
