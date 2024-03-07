9d25ca0e9113630a5447f04b13fee8276bc3f4f7
Arianna Vega <avega@equaldata.com>
2024-03-07 11:00:23 -0400

    fix 2

--- clase-4/utils.js
+++ clase-4/utils.js
@@ -1,3 +1,8 @@
-import { createRequire } from 'node:module';
+// One way to read a json in ESModules
+// import fs from 'node:fs'
+// const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
+
+// Another way to read a json in ESModules (recommended)
+import { createRequire } from 'node:module'
 const require = createRequire(import.meta.url)
 export const readJSON = (path) => require(path)
